98d5944da2642f46d4463ec9cd814daa42784103
Cameron Watts <wattscameron26@gmail.com>
2020-10-23 00:33:23 -0700

    Added header for timers

---
+++ lib/TIM.h
@@ -0,0 +1,7 @@
+#ifndef STM32_TIM
+#define STM32_TIM
+
+#define TIM2_CR1                (volatile uint32_t*) 0x40000000     // Timer control register 1
+#define TIM2_CNT                (volatile uint32_t*) 0x40000014
+
+#endif // STM32_TIM
